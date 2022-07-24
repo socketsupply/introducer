83ae6620dafc700d89d0cb73eaa716a96fdf3e2e
Dominic Tarr <dominic.tarr@gmail.com>
2022-07-25 10:15:01 +1200

    test coverage for big swarms

--- test/index.js
+++ test/index.js
@@ -152,3 +152,67 @@ test('swarm2', function (t) {
 
   t.end()
 })
+
+
+function idToAddress(id) {
+  return [
+    Number.parseInt(id.substring(0, 2), 16),
+    Number.parseInt(id.substring(2, 4), 16),
+    Number.parseInt(id.substring(4, 6), 16),
+    Number.parseInt(id.substring(6, 8), 16)
+  ].map(String).join('.')
+}
+
+test('swarmN', function (t) {
+  var swarm = createId('test swarm')
+  var network = new Network()
+//  var natD = new IndependentNat('42.')
+  var client
+  network.add(A, new Node(createPeer(new Introducer({id: ids.a}))))
+  network.add(B, new Node(createPeer(new Introducer({id: ids.b}))))
+
+  var peers = []
+  var N = 100
+  for(var i = 0; i < N; i++) {
+    var id = createId('id:'+i)
+    var address = idToAddress(id)
+    var natN = new IndependentNat(/^\d+\./.exec(address)[1])
+
+    network.add(address, natN)
+
+//    console.log(address)
+    natN.add(address, new Node(createPeer(peers[i] = new Peer({id: id, ...intros}) )))
+  //  peers[0].join(swarm)
+
+  }
+  network.iterate(-1)
+  //the network simulator doesn't set send/timer until it calls init,
+  //and i think that is something I could fix but that's how it is now so this
+  //makes these tests work, and they'll still work after I fix that problem...
+  for(var i = 0; i < N; i++)
+    peers[i].join(swarm)
+  network.iterate(-1)
+
+  for(var i = 0; i < N; i++) {
+    console.log(peers[i])
+    var pc = Object.keys(peers[i].peers).length
+    var sc = Object.keys(peers[i].swarm[swarm]).length
+    t.ok(pc >= 3, `peers[${i}] has at least 3 peers, got ${pc}`)
+    t.ok(sc >= 3, `peers[${i}] has at least 3 swarm, got: ${sc}`)
+  }
+
+/*
+  t.equal(peerD.nat, "easy")
+  peerD.join(swarm)
+  network.iterate(-1)
+  peerE.join(swarm)
+  network.iterate(-1)
+  console.log(peerE)
+  t.ok(peerE.swarm, 'peer has swarm object')
+  t.ok(peerE.swarm[swarm], 'peer has swarm key')
+  t.ok(peerE.peers[peerD.id])
+  t.ok(peerD.peers[peerE.id])
+*/
+//  console.log(peers[5])
+  t.end()
+})
