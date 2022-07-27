a428923e26cc892be90ab8efde11e928e74a90cb
Dominic Tarr <dominic.tarr@gmail.com>
2022-07-28 01:14:00 +1200

    test script for wrap

---
+++ test/wrap.js
@@ -0,0 +1,23 @@
+
+
+var wrap = require('../wrap')
+
+wrap({
+  init: () => {},
+  on_ping (msg, addr, port) {
+    this.send({type:'pong', addr}, addr, port)
+  }
+
+}, [1234])
+
+var client = {
+  on_pong () {
+    console.log("PONG")
+    
+  }
+}
+
+wrap(client, [1235])
+console.log(client)
+client.send( {type:'ping'}, {address:'127.0.0.1', port: 1234})
+
