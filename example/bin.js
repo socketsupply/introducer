f2f56a846349b2245a035623809770b3dc36f1a8
Dominic Tarr <dominic.tarr@gmail.com>
2022-08-05 11:47:40 +1200

    consistent dep passing style

---
+++ example/bin.js
@@ -0,0 +1,73 @@
+const udp = require('dgram')
+const crypto = require('crypto')
+const fs = require('fs')
+const path = require('path')
+
+const Demo = require('./chat')
+const Config = require('../lib/config')
+const Wrap = require('../wrap')(require('dgram'))
+const Multicast = require('../lib/multicast')(udp)
+const util = require('../util')
+
+function main (argv) {
+  const config = Config({ appname: 'introducer-chat' }, crypto, fs, path)
+  const cmd = argv[0]
+  const swarm = util.createId('test swarm')
+  /* multicast
+    to find other peers on the local network,
+    we need a parallel multicast system.
+    it appears that a socket cannot be used for both
+  */
+
+  if (cmd === 'introducer') {
+    Wrap(udp, new Introducer(), [config.port])
+    console.log(config.id)
+    return
+  }
+
+  const peer = new Demo({ swarm, ...config })
+  peer.on_change = (msg) => {
+    console.log(msg.id.substring(0, 8), msg.ts, msg.content)
+  }
+
+  //detect the nat type and exit
+  if(cmd === 'nat') {
+    setTimeout(()=>{
+      console.log('failed to detect nat, offline?')
+      process.exit(1)
+    }, 3_000)
+    peer.on_nat = (n) => {
+      console.log(n)
+      process.exit(0)
+    }
+  }
+
+  Wrap(peer, [config.port])
+
+  process.stdin.on('data', function (data) {
+    const c = peer.chat({ ts: Date.now(), content: data.toString() })
+    console.log('DATA', data.toString(), c)
+  })
+
+  // broadcast our presense on local network.
+  // our address is detectable.
+  // but include our port, because message will be received on multicast
+  // only port which won't receive direct packets.
+  Multicast(6543, function () {
+    return JSON.stringify({ type: 'broadcast', id: config.id, port: config.port, ts: Date.now() })
+  }, function (data, addr) {
+    // when we detect a peer, just ping them,
+    // that will trigger the other peer management stuff.
+    // hmm, also need to join swarms with them?
+    const msg = JSON.parse(data.toString())
+    if (msg.id === peer.id) return // ignore our own messages
+    peer.ping({ address: addr.address, port: msg.port })
+
+    // mark as a local peer,
+    // when you join a swarm, also message local peers to join the swarm
+    // (just incase they are in it, cheap to message locally)
+  })
+}
+
+if(!module.parent)
+  main(process.argv.slice(2))
--- wrap.js
+++ wrap.js
@@ -1,3 +1,4 @@
+
 const debug = process.env.DEBUG ? function (...args) { console.log(...args) } : function () {}
 
 function isPort (p) {
@@ -17,53 +18,55 @@ const json = {
   decode: (buf) => JSON.parse(buf.toString())
 }
 
-function wrap (dgram, peer, ports, codec = json) {
-  const bound = {}
+module.exports = (UDP) => { 
 
-  peer.send = (msg, addr, from_port) => {
-    debug('send', msg, addr)
-    const sock = bind(from_port)
-    if (addr === '255.255.255.255') sock.setBroadcast(true)
-    sock.send(codec.encode(msg), addr.port, addr.address)
-  }
+  return function wrap (peer, ports, codec = json) {
+    const bound = {}
 
-  peer.timer = (delay, repeat, fn) => {
-    function interval () {
-      if (fn() === false) clearInterval(int)
+    peer.send = (msg, addr, from_port) => {
+      debug('send', msg, addr)
+      const sock = bind(from_port)
+      if (addr === '255.255.255.255') sock.setBroadcast(true)
+      sock.send(codec.encode(msg), addr.port, addr.address)
     }
-    if (!delay && fn() !== false && repeat) { setInterval(interval, repeat) } else {
-      setTimeout(function interval () {
-        if (fn() !== false && repeat) { const int = setInterval(interval, repeat) }
-      }, delay)
+
+    peer.timer = (delay, repeat, fn) => {
+      function interval () {
+        if (fn() === false) clearInterval(int)
+      }
+      if (!delay && fn() !== false && repeat) { setInterval(interval, repeat) } else {
+        setTimeout(function interval () {
+          if (fn() !== false && repeat) { const int = setInterval(interval, repeat) }
+        }, delay)
+      }
     }
-  }
 
-  function onMessage (msg, addr, port) {
-    debug('recv', msg)
-    if (isString(msg.type) && isFunction(peer['on_' + msg.type])) { peer['on_' + msg.type](msg, addr, port) }
-  }
+    function onMessage (msg, addr, port) {
+      debug('recv', msg)
+      if (isString(msg.type) && isFunction(peer['on_' + msg.type])) { peer['on_' + msg.type](msg, addr, port) }
+    }
 
-  // support binding anynumber of ports on demand (necessary for birthday paradox connection)
-  function bind (p) {
-    if (!isPort(p)) { throw new Error('expected port, got:' + p) }
-    if (bound[p]) return bound[p]
-    debug('bind', p)
-    return bound[p] = dgram
-      .createSocket('udp4')
-      .bind(p)
-      // .on('listening', function () { this.setBroadcast(true) })
-      .on('message', (data, addr) => {
-        onMessage(codec.decode(data), addr, p)
-      })
-      .on('error', (err) => {
-        if (err.code === 'EACCES') {
-          console.log('could not bind port:' + err.port)
-        }
-      })
-  }
+    // support binding anynumber of ports on demand (necessary for birthday paradox connection)
+    function bind (p) {
+      if (!isPort(p)) { throw new Error('expected port, got:' + p) }
+      if (bound[p]) return bound[p]
+      debug('bind', p)
+      return bound[p] = UDP
+        .createSocket('udp4')
+        .bind(p)
+        // .on('listening', function () { this.setBroadcast(true) })
+        .on('message', (data, addr) => {
+          onMessage(codec.decode(data), addr, p)
+        })
+        .on('error', (err) => {
+          if (err.code === 'EACCES') {
+            console.log('could not bind port:' + err.port)
+          }
+        })
+    }
 
-  if (peer.init) peer.init()
-  if (ports) ports.forEach(bind)
+    if (peer.init) peer.init()
+    if (ports) ports.forEach(bind)
 }
 
-module.exports = wrap
+}
