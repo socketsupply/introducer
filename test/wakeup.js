7f11b06b2e59aa050b47649a4257fabf12234d0c
Dominic Tarr <dominic.tarr@gmail.com>
2022-08-17 22:40:56 +1200

    working on tests for wakeup

---
+++ test/wakeup.js
@@ -0,0 +1,125 @@
+
+const test = require('tape')
+const crypto = require('crypto')
+const { EventEmitter } = require('events')
+
+const { createId: _createId } = require('../util')
+const Chat = require('../example/chat')
+const Introducer = require('../introducer')
+const createId = (...args) => _createId(crypto, ...args)
+const swarm = createId('test swarm')
+const { Node, Network, IndependentNat, IndependentFirewallNat, DependentNat } = require('@socketsupply/netsim')
+// var nc = require('../')
+
+const A = '1.1.1.1'
+const B = '2.2.2.2'
+const C = 'cc.cc.cc.cc'
+const D = '42.4.4.4'
+const E = '52.5.5.5'
+
+const d = '42.4.4.42'
+const e = '52.5.5.52'
+
+const P = ':3489'
+
+const ids = {}; let id_count = 0
+
+for (let i = 0; i < 1000; i++) {
+  const id = createId('_' + i)
+  if (!ids[id[0]]) {
+    ids[id[0]] = id
+    id_count++
+  }
+  if (id_count == 16) break
+}
+
+function createPeer (p) {
+  return function (send, timer, node) {
+    p.send = send
+    p.timer = function (delay, repeat, fn) {
+      timer(delay, repeat, () => {
+        p.localAddress = node.address
+        return fn()
+      })
+    }
+    p.localAddress = node.address
+    // console.log('timer', timer.toString())
+    if (p.init) p.init()
+    return function (msg, addr, port) {
+      const type = msg.type
+      if (p['on_' + type]) p['on_' + type](msg, addr, port)
+      else if (p.on_msg) p.on_msg(msg, addr, port)
+    }
+  }
+}
+function createNatPeer (network, id, address_nat, address, Nat) {
+  const prefix = /^\d+\./.exec(address_nat)[1]
+  const nat = new Nat(prefix)
+  let peer = new Chat({ id, ...intros, keepalive: 60_000, swarm })
+  peer.on_change = ()=>{}
+  let node = new Node(createPeer(peer))
+  network.add(address_nat, nat)
+  nat.add(address, node)
+  console.log("NODE", node.sleep)
+  return [peer, nat, node]
+}
+
+
+const intros = {
+  introducer1: { id: ids.a, address: A, port: 3456 },
+  introducer2: { id: ids.b, address: B, port: 3456 }
+}
+
+test('swarm with 1 easy 1 hard', function (t) {
+  const swarm = createId('test swarm')
+  const network = new Network()
+  let client
+  let intro
+  network.add(A, new Node(createPeer(intro = new Introducer({ id: ids.a, keepalive: 5_000 }))))
+  network.add(B, new Node(createPeer(new Introducer({ id: ids.b, keepalive: 5_000 }))))
+
+  const [peer_easy, nat_easy] = createNatPeer(network, createId('id:easy'), '1.2.3.4', '1.2.3.42', IndependentFirewallNat)
+  const [peer_hard, nat_hard, node_hard] = createNatPeer(network, createId('id:hard'), '5.6.7.8', '5.6.7.82',
+IndependentFirewallNat)
+// DependentNat)
+
+  network.iterateUntil(10_000)
+  peer_easy.join(swarm)
+  peer_hard.join(swarm)
+
+  network.iterateUntil(20_000)
+
+  // the introducer should know about everyone's nats now.
+  t.equal(intro.peers[peer_easy.id].nat, 'easy')
+  t.equal(intro.peers[peer_hard.id].nat, 'easy')
+
+  t.ok(peer_easy.peers[peer_hard.id], 'easy peer knows hard peer')
+  t.ok(peer_hard.peers[peer_easy.id], 'hard peer knows easy peer')
+
+  network.iterateUntil(30_000)
+
+  console.log(node_hard.sleep)
+  node_hard.sleep(true)
+
+  peer_easy.chat({content: 'missing', ts: 40_000}) //node_hard will not see this
+  
+  network.iterateUntil(2*60_000)
+
+  console.log("WAKEUP")
+  node_hard.sleep(false)
+
+  //another half minute is enough to wake up
+  network.iterateUntil(10*60_000)
+
+  console.log(peer_easy.peers[peer_hard.id])
+  peer_easy.chat({content: 'expected', ts: 11*60_000}) //node_hard will not see this
+
+  network.iterateUntil(12*60_000)
+
+  console.log(peer_easy.messages)
+  console.log(peer_hard.messages)
+
+  //TODO: peer timers need to corectly integrate with netsim, and nat simulators need timeouts
+
+  t.end()
+})
