5f798d568913002fdaa51435c5e88f56e2e36a91
Dominic Tarr <dominic.tarr@gmail.com>
2022-08-17 14:27:21 +1200

    reconnection tests

--- test/reconnect.js
+++ test/reconnect.js
@@ -116,3 +116,51 @@ test('swarm with 1 easy 1 hard', function (t) {
 
   t.end()
 })
+
+test('disconnect, reconnect', function (t) {
+
+  const swarm = createId('test swarm')
+  const network = new Network()
+  let client
+  let intro
+  network.add(A, new Node(createPeer(intro = new Introducer({ id: ids.a, keepalive: 5_000 }))))
+  network.add(B, new Node(createPeer(new Introducer({ id: ids.b, keepalive: 5_000 }))))
+
+  const [peer_easy, nat_easy] = createNatPeer(network, createId('id:easy'), '1.2.3.4', '1.2.3.42', IndependentFirewallNat)
+  const [peer_hard, nat_hard, node_hard] = createNatPeer(network, createId('id:hard'), '5.6.7.8', '5.6.7.82', DependentNat)
+
+  network.iterateUntil(10_000)
+  peer_easy.join(swarm)
+  peer_hard.join(swarm)
+
+  network.iterateUntil(20_000)
+
+  // the introducer should know about everyone's nats now.
+  t.equal(intro.peers[peer_easy.id].nat, 'easy')
+  t.equal(intro.peers[peer_hard.id].nat, 'hard')
+
+  t.ok(peer_easy.peers[peer_hard.id], 'easy peer knows hard peer')
+  t.ok(peer_hard.peers[peer_easy.id], 'hard peer knows easy peer')
+
+  // console.log(nat_hard)
+
+  console.log(peer_easy.peers[peer_hard.id])
+  console.log(peer_hard.peers[peer_easy.id])
+
+  var new_nat = new IndependentFirewallNat()
+
+  network.add('2.4.6.8', new_nat)
+  new_nat.add('2.4.6.80', node_hard)
+
+  network.iterateUntil(30_000)
+
+  console.log(peer_hard)
+  t.equal(peer_hard.localAddress, '2.4.6.80')
+  t.equal(peer_hard.publicAddress, '2.4.6.8', 'public address is correct')
+  t.equal(peer_easy.peers[peer_hard.id].address, '2.4.6.8', 'other peer knows the new public address')
+  //now, move one peer to another address, iterate, and check they regain connection.
+
+
+  t.end()
+
+})
