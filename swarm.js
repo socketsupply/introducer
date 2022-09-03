e4294ceb547d0eaed98584f9af8c41ee9f5dbab3
Dominic Tarr <dominic.tarr@gmail.com>
2022-09-03 15:21:44 +1200

    redesign swarm to manage multiple different data structures and swarms

--- swarm.js
+++ swarm.js
@@ -11,10 +11,20 @@ function equalAddr (a, b) {
   return a && b && a.address === b.address && a.port === b.port
 }
 
+function append(item, ary=[]) {
+  //check we don't already have it
+  for(var i = 0; i < ary.length; i++)
+    if(ary[i].ts === item.ts) return null
+  ary.push(item)
+  return ary
+}
+
 module.exports = class Demo extends Swarm {
   constructor (opts) {
     super(opts)
     this.swarms = {}
+    this.data = {}
+    this.handlers = {}
     if(!isId(opts.id))
       throw new Error('peer id *must* be provided')
     if(isId(opts.swarm))
@@ -28,19 +38,46 @@ module.exports = class Demo extends Swarm {
 
   }
 
-  chat ({ content, ts = Date.now() }) {
-    const msg = { type: 'chat', id: this.id, swarm: this.swarm, content, ts }
-    this.messages.push(msg)
-    this.on_change(msg, this.messages)
-    return this.broadcast(msg)
+  //create a data model, this takes an id, plus a function to update the datamodel
+
+  createModel(swarm, reduce=append) {
+    this.handlers[swarm] = reduce
+    //defer, incase that this instance hasn't been wrapped yet
+    if(this.nat) this.join(swarm)
+    return this
+  }
+
+  getModel(swarm) {
+    return this.data[swarm]
+  }
+
+  update(msg, addr = null) {
+    var data = this.data[msg.swarm] 
+    var _update
+    console.log("UPDTATATATAH", msg)
+    if(this.handlers[msg.swarm]) {
+      _update = this.handlers[msg.swarm](msg, data)
+      console.log('update', data, msg, _update)
+      //if we already have this message, do not notify or rebroadcast
+      if(_update !== null) {
+        this.data[msg.swarm] = _update
+        console.log('updated', this.data)
+        this.on_change(msg, this.messages)
+        this.broadcast(msg, addr)
+      }
+    }
+    else {
+      debug(1, "update was missing swarm field:", msg)
+    }
+  }
+
+  chat ({ content, ts = Date.now(), swarm }) {
+    this.update({ type: 'chat', id: this.id, swarm, content, ts })
   }
 
   // when a message is received, if it is new, broadcast it to our other peers.
   on_chat (msg, addr, port) {
-    if(this.messages.find(_msg => _msg.ts == msg.ts && _msg.content == msg.content)) return
-    this.messages.push(msg)
-    this.on_change(msg, this.messages)
-    this.broadcast(msg, addr)
+    this.update(msg, addr)
   }
 
   on_nat () {
@@ -52,8 +89,8 @@ module.exports = class Demo extends Swarm {
     debug(1, 'have nat:', this.nat, info)
 
     //or just request to all peers to join this swarm
-    if(this.swarm)
-      this.join(this.swarm)
+    for(var swarm in this.handlers)
+      this.join(swarm)
   }
 
   on_error (msg) {
