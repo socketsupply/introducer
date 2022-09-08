cd1f1c6dfd13791630c2200b32673ca9e0cc6709
Dominic Tarr <dominic.tarr@gmail.com>
2022-09-08 21:50:24 +1200

    working on reliable data structures

--- reliable.js
+++ reliable.js
@@ -4,42 +4,71 @@ var np = require('new_protocol')
 
 class ReliablePeer extends Swarm {
 
+  constructor (opts) {
+    super(opts)
+    this.waiting = []
+  }
+  
   //receive flooded message
-  on_update1 (msg, addr, port) {
+  on_update (msg, addr, port) {
     var r = np.update(this.state, msg.content, msg.ts)
     if(r === false) {
-      //send just back to this peer? and other peers maybe?
-      //hmm, if we used EBT style pruning, then the secondary peers could be asked after a delay...
-      // **EBT is an optimization that can come later**
-      this.send({
-        type: 'request', swarm,
-        content: np.request(this.state, msg.prev)
-      },
-        addr, port
-      )
+      //request missing messages
+      this.request(msg, addr)
     } else if (r === true) {
       //already have this message, so do nothing
       //OR, ebt prune this peer?
     } else {
       //new message, broadcast to everyone in swarm (that hasn't pruned us)
+      //XXX maybe repeats should be handled differently, if I had to request this message again,
+      //    don't broadcast it. (probably everyone got it directly already?) 
       this.swarmcast(msg, msg.swarm, addr)
+      this.on_change(msg, this.state)
+      if(this.waiting.length) {
+        //check to see if any waiting messages can now be applied
+        while(i < this.waiting.length) {
+          var msg = this.waiting[i]
+          r = np.update(this.state, msg.content, msg.ts) //is msg.ts correct?
+          if(r == false) {
+            i ++
+            continue
+          }
+          this.waiting.splice(i, 1)
+          if('object' === typeof r) {
+            this.state = r  
+          }
+        }
+      }
     }
-    //super.on_chat(msg, addr, port)
   }
 
-  update1 (change) {
-    
+  update (content, swarm, ts) {
+    var msg = create(this.state, content, ts)
+    this.state = update(this.state, msg)
+    this.swarmcast(msg, swarm)
+    this.on_change(msg, this.state)
+  }
+
+  request (msg, from) {
+    this.waiting.push(msg)
+    this.send({
+        type: 'request', swarm,
+        content: np.request(this.state, msg.prev)
+      },
+      from, from.outport
+    )
   }
 
   on_request (msg, addr, port) {
     var a = np.missing(state, msg.content.has, msg.content.needs)
     if(a.length) {
-      for(var i = 0; i < a.length; i++)
-        this.send({type: 'chat', id: this.id, swarm: msg.swarm, content: a[i]}, addr, port)
+      for(var i = 0; i < a.length; i++) {
+        this.send({type: 'update', id: this.id, swarm: msg.swarm, content: a[i]}, addr, port)
+      }
     }
     else {
       //error that do not have the message?
-
+      
     }
   }
 
