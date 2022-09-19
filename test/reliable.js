78b2a97bca5feac5f1718ef59ef7f2d4e3896f21
Dominic Tarr <dominic.tarr@gmail.com>
2022-09-20 00:36:04 +1200

    test coverage for adding messages while offline

--- test/reliable.js
+++ test/reliable.js
@@ -157,3 +157,67 @@ test('swarm, connect then expect to receive updates', function (t) {
   t.equal(received.length, 2)
   t.end()
 })
+
+
+
+test.only('swarm, make updates while offline, before connection', function (t) {
+  const swarm = createId('test swarm')
+  const network = new Network()
+  const natD = new IndependentNat('42.')
+  const natE = new IndependentNat('52.')
+  const natF = new IndependentNat('62.')
+  var peerD, peerE, peerF, swarmD, swarmE, swarmF
+
+  network.add(A, new Node(new Introducer({ id: ids.a })))
+  network.add(B, new Node(new Introducer({ id: ids.b })))
+  network.add(D, natD)
+  network.add(E, natE)
+  network.add(F, natF)
+  natD.add(d, new Node(peerD = new Swarms({ id: ids.d, ...intros })))
+  var swarmD = peerD.createModel(swarm, new Reliable(swarm))
+
+  natE.add(e, new Node(peerE = new Swarms({ id: ids.e, ...intros })))
+  var swarmE = peerE.createModel(swarm, new Reliable(swarm))
+
+  swarmD.update('HELLO_D1', network.queue.ts)
+//  swarmD.update('HELLO_D2', swarm, network.queue.ts+1)
+  swarmE.update('HELLO_E2', network.queue.ts+2)
+
+  network.iterate(-1)
+
+  t.equal(peerD.nat, 'easy')
+  network.iterateUntil(1000)
+
+  peerD.join(swarm)
+  peerE.join(swarm)
+  network.iterateUntil(2000)
+  t.ok(peerE.peers[peerD.id])
+  t.ok(peerD.peers[peerE.id])
+  network.iterateUntil(3000)
+
+  t.deepEqual(swarmE.data, swarmD.data)
+//  t.deepEqual(swarmE.data, swarmD.data)
+  console.log(swarmD.data)
+  //console.log(swarmE.data)
+//  network.add(F, natF)
+  /*
+  natF.add(f, new Node(peerF = new Swarms({ id: ids.f, ...intros })))
+  //this will trigger a join
+  network.iterateUntil(4000)
+  t.equal(peerF.nat, 'easy')
+
+//  peerF.join(swarm)
+  var swarmF = peerF.createModel(swarm, new Reliable(swarm))
+  var received = []
+  swarmF.on_change = (msg) => received.push(msg)
+  network.iterateUntil(5000)
+  //a new peer has joined, but it doesn't know there is any messages yet.
+  //sending a new message shows it that soemthing is missing so it requests the old messages.
+//  swarmD.update('welcome', swarm, network.queue.ts)
+  //network.iterateUntil(6000)
+  t.deepEqual(swarmF.data, swarmD.data)
+  t.equal(received.length, 2)
+  */
+  t.end()
+})
+
