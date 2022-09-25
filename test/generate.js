1ea5c819579731af2053921941b464d8263137b3
Dominic Tarr <dominic.tarr@gmail.com>
2022-09-26 12:36:03 +1300

    the first generated test

--- test/generate.js
+++ test/generate.js
@@ -1,7 +1,53 @@
-var {Node, Network} = require('@socketsupply/netsim')
+/**
+
+GENERATED RANDOM TESTING SCRIPT
+
+with this script we run tests many times with deterministic random event ordering
+
+In a distributed system, the order of packet delivery, and even wether or not they arrive,
+is not predictable. So to make a reliable system, we must ensure that the system still works
+under all possible orders.
+
+Exaustively searching all possible orders would be more efficient, but complex.
+But it's very simple to just randomly sample the space of possible orderings.
+
+Run each test many times, with ordering randomized, but deterministic seed.
+Then, collect failures by seed, and allow rerunning single tests with particular seed for debugging.
+
+to run all tests:
+```
+> node generate.js
+```
+will produce output like this:
+```
+{
+  [result]:[seeds...]
+}
+```
+`result` is either `true`, `false` or an error message.
+
+to rerun a tests with seed 7, 
+
+```
+> SEED=7 node generate.js
+
+```
+
+**/
+
+
+var MT = require('rng').MT
+var mt = new MT(7)
+function random () {
+  return mt.random()
+}
+Math.random = random
+var {Node, Network} = require('@socketsupply/netsim/base')(random)
+
 var Swarms = require('../swarms')
 var Reliable = require('../swarm/reliable')
 var Introducer = require('../introducer')
+var deepEqual = require('deep-equal')
 //generate a swarm.
 //any number of peers.
 //then peers emit messages
@@ -15,7 +61,7 @@ function first (obj) {
 }
 
 function rand (i) {
-  return Math.round(Math.random()*i)
+  return Math.round(random()*i)
 }
 
 function createId () {
@@ -32,44 +78,83 @@ function randomAddress () {
 function generatePeer (network, create) {
   network.add(randomAddress(), create(createId()))
 }
-
-var intros = {
-  introducer1: {
-    id: createId(),
-    address: randomAddress(),
-    port: 3456
-  },
-  introducer2: {
-    id: createId(),
-    address: randomAddress(),
-    port: 3456
+function createIntros () {
+  return {
+    introducer1: {
+      id: createId(),
+      address: randomAddress(),
+      port: 3456
+    },
+    introducer2: {
+      id: createId(),
+      address: randomAddress(),
+      port: 3456
+    }
   }
 }
-var network = new Network()
-var N = 3
 var swarm = createId()
-var peers = {}
 
-for(var k in intros) {
-  network.add(intros[k].address, new Node(new Introducer({id: intros[k].id})))
+function generate (network, N, swarm) {
+  var peers = {}
+
+  var intros = createIntros()
+  for(var k in intros) {
+    network.add(intros[k].address, new Node(new Introducer({id: intros[k].id})))
+  }
+
+  for(var i = 0; i < N; i++)
+    generatePeer(network, (id) => {
+      var p = new Swarms({id, ...intros})
+      peers[id] = p
+      p.createModel(swarm, new Reliable())
+      return new Node(p)
+    })
+
+  return peers
 }
 
-for(var i = 0; i < N; i++)
-  generatePeer(network, (id) => {
-    var p = new Swarms({id, ...intros})
-    peers[id] = p
-    p.createModel(swarm, new Reliable())
-    return new Node(p)
-  })
 
-console.log(peers)
-console.log(network)
-var p = first(peers)
+//console.log(peers)
+//console.log(network)
 
-p.handlers[swarm].update('hello', 100)
-network.iterateUntil(1000)
+function test_eventual_consistency (peers) {
+  var p = first(peers)
 
-var data = {}
-for(var k in peers)
-  data[k] = peers[k].handlers[swarm].data
-console.log(data)
+  p.handlers[swarm].update('hello', 100)
+  try {
+  network.iterateUntil(2000)
+  } catch (err) {
+    return {data, result: false, error: err}
+  }
+  var data = {}
+  for(var k in peers)
+    data[k] = peers[k].handlers[swarm].data
+//  console.log(data)
+  for(var k in data) {
+    if(k != p.id) {
+      if(!deepEqual(data[k], data[p.id]))
+        return {data, result: false}
+    }
+  }
+  return {data, result: true}
+}
+
+if(+process.env.SEED) {
+    mt = new MT(+process.env.SEED)
+    var network = new Network()
+    var {data, result, error} = test_eventual_consistency(generate(network, 2, swarm))
+    console.log(result ? "PASS" : "FAIL", error)
+    console.log(JSON.stringify(data, null, 2))
+}
+else {
+  var results = {}
+  for(var i = 0; i < 100; i++) {
+    mt = new MT(i)
+    var network = new Network()
+    var {data, result, error} = test_eventual_consistency(generate(network, 2, swarm))
+    var name = error ? error.message : result
+    ;(results[name] = results[name] || []).push(i)
+//    console.log({seed: i, data, result, error})
+  }
+  console.log(results)
+}
